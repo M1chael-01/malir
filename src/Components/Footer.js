4c44a261ed578091f3dea1f4e22beb0fce55cb5e
Michael Tvrdík <tvrdikmichael400@gmail.com>
2025-09-15 10:18:57 +0200

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,21 +3,23 @@ import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-100 text-black py-12">
-      <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
+    <footer className="bg-slate-100 text-black py-12 w-full">
+      <div className="w-full max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
         {/* Text */}
-        <div className="flex flex-col md:flex-row gap-4 md:gap-6 items-center md:items-start">
-          <p className="text-center md:text-left text-gray-600">
-            Jedná se o fiktivní web. Vytvořil <span className="font-bold">Michael Tvrdík</span>. Údaje nejsou pravdivé
+        <div className="flex flex-col md:flex-row gap-4 md:gap-6 items-center md:items-start text-center md:text-left">
+          <p className="text-gray-600">
+            Jedná se o fiktivní web. Web nesbírá osobní údaje ani nenabízí služby. Údaje nejsou pravdivé.
           </p>
-          <p className="text-center md:text-left text-gray-500 text-sm">
-            Všechny fotografie jsou ilustrativní a pocházejí ze <a href="https://www.freepik.com" className="underline hover:text-blue-500">Freepik</a>.
-          
-            
+          <p className="text-gray-600">
+            <span className="font-bold">Vytvořil:</span> Michael Tvrdík, tvrdikmichael@gmail.com
+          </p>
+          <p className="text-gray-500 text-sm">
+            Všechny fotografie jsou ilustrativní a pocházejí ze{' '}
+            <a href="https://www.freepik.com" className="underline hover:text-blue-500">
+              Freepik
+            </a>.
           </p>
         </div>
-
-       
       </div>
     </footer>
   );
